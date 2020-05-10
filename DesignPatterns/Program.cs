fa68f5bdbbda2c9dba4d0414167510f496b49fc2
MCCshreyas <shreyasjejurkar123@live.com>
2020-05-10 13:07:44 +0530

    Singleton done

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -2,11 +2,40 @@
 
 namespace DesignPatterns
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Logger obj1 = Logger.GetInstance();
+            Logger obj2 = Logger.GetInstance();
+
+            // Both will be same as they are same object return by GetInstance method
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            Console.ReadLine();
+        }
+    }
+
+    /// <summary>
+    /// Singleton logger class
+    /// </summary>
+    public class Logger
+    {
+        private static Logger logger;
+
+        // Make constructor private so that outisde cannot initialize it
+        private Logger()
+        {
+        }
+
+        public static Logger GetInstance()
+        {
+            if(logger == null)
+            {
+                logger = new Logger();
+            }
+            return logger;
         }
     }
 }
